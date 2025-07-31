bf86841182e193ef01d47762c0dba5100d050703
Hemlata Patel <ravpatel1304@gmail.com>
2025-07-31 16:39:46 +0530

    New

--- frontend/src/pages/AttendanceForm.jsx
+++ frontend/src/pages/AttendanceForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../api/api';
-import '../styles/attendanceForm.css';
+import '../styles/AttendanceForm.css';
 
 const AttendanceForm = () => {
   const [students, setStudents] = useState([]);
